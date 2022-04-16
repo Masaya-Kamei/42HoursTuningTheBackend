76a60f64d4c1609feec453fbe30260f14d379876
root <root@env15-olive.bk4edyqde52u3kc3p4qmkrmh1d.lx.internal.cloudapp.net>
2022-04-16 11:58:49 +0000

    index を追加

--- development/mysql/sql/V1.sql
+++ development/mysql/sql/V1.sql
@@ -1,1 +1,11 @@
 ALTER TABLE session ADD INDEX index_on_value(value);
+ALTER TABLE user ADD INDEX index_on_user_id(user_id);
+ALTER TABLE group_info ADD INDEX index_on_group_id(group_id);
+ALTER TABLE group_member ADD INDEX index_on_user_id(user_id);
+ALTER TABLE record_last_access ADD INDEX index_on_id(user_id, record_id);
+ALTER TABLE category ADD INDEX index_on_category_id(category_id);
+ALTER TABLE category_group ADD INDEX index_on_group_id(group_id);
+ALTER TABLE file ADD INDEX index_on_file_id(file_id);
+ALTER TABLE record ADD INDEX index_on_record_id(record_id);
+ALTER TABLE record_item_file ADD INDEX index_on_lr_id(linked_record_id);
+ALTER TABLE record_comment ADD INDEX index_on_lr2_id(linked_record_id);
